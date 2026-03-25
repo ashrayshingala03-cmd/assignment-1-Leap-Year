25c09d7fbc3aaa5dfcb831ee4a02e5d50302672c
ashrayshingala03-cmd <ashrayshingala03@gmail.com>
2026-03-25 15:09:20 +0530

    Update leapyear.c

--- leapyear.c
+++ leapyear.c
@@ -4,15 +4,13 @@ Write a program in C to check wether a given year is a leap year or not
 
 #include <stdio.h>
 int main()
-    
-    {
+{
     int year;
     printf("Enter a year:");
     scanf("%d", &year);
-    
     if ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0)) {
-    printf("%d is a leap year \n", year);}
+        printf("%d is a leap year \n", year);}
     else {
-    printf("%d is not a leap year \n", year) }
+        printf("%d is not a leap year \n", year) }
     return 0;
-    }
+}
